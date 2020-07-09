6e8ff9b415336be2abded7168679606fb06c9c18
Stephen Cefali <stephencefali@gmail.com>
2020-07-09 11:03:39 -0700

    init

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,6 @@ process.env.NODE_ENV === "production" &&
     dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
   });
 
-
 export default function App({ Component, pageProps, err }) {
   // Workaround for https://github.com/vercel/next.js/issues/8592
   return <Component {...pageProps} err={err} />;
