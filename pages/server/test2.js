9afbafee657bde239e8acb43b4d0709b90043da7
Stephen Cefali <stephencefali@gmail.com>
2020-07-16 12:56:14 -0700

    udpdat

--- pages/server/test2.js
+++ pages/server/test2.js
@@ -1,7 +1,15 @@
+import * as Sentry from '@sentry/node'
+
 const Test2 = () => <h1>Server Test 2</h1>
 
 export async function getServerSideProps() {
-  throw new Error('Server Test 2')
+  try {
+    throw new Error('Server Test 2')
+  } catch (err){
+    Sentry.captureException(err)
+    await Sentry.flush(2000);
+    throw err
+  }
 }
 
 export default Test2
