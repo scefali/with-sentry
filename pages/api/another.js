0c0870640556be668d170e0cae9e1ecf9f20c621
Stephen Cefali <stephencefali@gmail.com>
2020-07-17 11:11:39 -0700

    update

--- pages/api/another.js
+++ pages/api/another.js
@@ -1,9 +1,9 @@
 import * as Sentry from "@sentry/node";
 
-process.env.NEXT_PUBLIC_SENTRY_DSN &&
-  Sentry.init({
-    dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-  });
+// process.env.NEXT_PUBLIC_SENTRY_DSN &&
+//   Sentry.init({
+//     dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+//   });
 
 function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
