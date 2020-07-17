bfa77fc548baf93ff179a6e3cf0db9822fb2c5b6
Stephen Cefali <stephencefali@gmail.com>
2020-07-17 10:44:50 -0700

    update

--- pages/server/test2.js
+++ pages/server/test2.js
@@ -2,6 +2,10 @@ import * as Sentry from '@sentry/node'
 
 const Test2 = () => <h1>Server Test 2</h1>
 
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 
 export async function getServerSideProps() {
   try {
@@ -9,7 +13,7 @@ export async function getServerSideProps() {
   } catch (err){
     console.error(err)
     Sentry.captureException(err)
-    await Sentry.flush(10000);
+    await sleep(4000);
     throw err
   }
 }
