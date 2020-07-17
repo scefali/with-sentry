5a376281d8ef69bdd0b73dcd8280ec160dbe111c
Stephen Cefali <stephencefali@gmail.com>
2020-07-17 11:23:03 -0700

    update

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,24 @@
 import * as Sentry from "@sentry/node";
+import Document, { Head, Main, NextScript } from 'next/document';
 
 process.env.NEXT_PUBLIC_SENTRY_DSN && 
   Sentry.init({
     dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
   });
+
+export default class MyDocument extends Document {
+
+  render() {
+    return (
+      <html>
+        <Head>
+          <title>My page</title>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+}
