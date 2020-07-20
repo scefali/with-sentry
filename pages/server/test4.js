b3c8b08f0f245ff4baf85e5d03a1d8fe3eacb903
Stephen Cefali <stephencefali@gmail.com>
2020-07-20 10:30:00 -0700

    update

--- pages/server/test4.js
+++ pages/server/test4.js
@@ -1,5 +1,5 @@
-const doAsyncWork = () => Promise.reject(new Error('Server Test 4'))
-doAsyncWork()
+// const doAsyncWork = () => Promise.reject(new Error('Server Test 4'))
+// doAsyncWork()
 
 const Test4 = () => <h1>Server Test 4</h1>
 
