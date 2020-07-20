fabf05de34f554f0a747b122f90753f6e1f94cf8
Stephen Cefali <stephencefali@gmail.com>
2020-07-20 09:56:58 -0700

    update

--- pages/api/another.js
+++ pages/api/another.js
@@ -1,16 +1,16 @@
 import * as Sentry from "@sentry/node";
 
-// process.env.NEXT_PUBLIC_SENTRY_DSN &&
-//   Sentry.init({
-//     dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-//   });
+process.env.NEXT_PUBLIC_SENTRY_DSN &&
+  Sentry.init({
+    dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+  });
 
 function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 export default async (req, res) => {
-  Sentry.captureException(new Error("Another"));
+  Sentry.captureException(new Error("My server error"));
   await Sentry.flush(2000)
-  res.status(200).json({ text: "Another" });
+  res.status(200).json({ text: "My server" });
 };
